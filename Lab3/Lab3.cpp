6a72dd8946b72013ef92ce5ded224e988268fb06
Fine Danil <danil2003.2043@gmail.com>
2020-11-14 15:07:17 +0300

    code gotov

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -10,9 +10,25 @@ int main()
 	Вывести его строку-описание вида «отрицательное четное число», 
 	«нулевое число»,
 	«положительное нечетное число» и т. д.*/
-	int number;
-	std::cin >> number;
+
+	setlocale(LC_ALL, "rus");
+	while (1) {
+		int number;
+		std::cin >> number;
+
+		if (number == 0)
+			std::cout << "нулевое ";
+		else {
+			if (number > 0)
+				std::cout << "положительное ";
+			else
+				std::cout << "отрицательное ";
+			if ((number & 1))
+				std::cout << "нечетное ";
+			else
+				std::cout << "четное ";
+		}
+		std::cout << "число \n";
+	}
 	
-
-
 }
